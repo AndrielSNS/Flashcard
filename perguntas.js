b88fcc246b455fc1c63f1b0aae74ca2a78958d96
AndrielSNS <00001104106309sp@al.educacao.sp.gov.br>
2024-11-27 20:02:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Html?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz com licença em Inglês?',
+    'Com licença em ingles é excuse me (RAI)'
+)
